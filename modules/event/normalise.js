ca515ed38398c1b94bf3bc3c80ade97af394926e
Stephen <stephband@cruncher.ch>
2025-10-17 04:20:48 +0200

    More normalisation

--- modules/event/normalise.js
+++ modules/event/normalise.js
@@ -15,14 +15,24 @@ function toUnicode(char) {
 export default function normalise(event) {
     switch (event[1]) {
         case "chord":
-            // Chord root
+            // Chord root number
             event[2] = toRootNumber(event[2]);
             // Chord extension
             event[3] = event[3].replaceAll(rflatsharp, toUnicode);
             break;
+        case "lyric":
+            // Warn user over pold version
+            console.warn('Old data contains "lyric" event, should be "text"');
+            // Change type to "text"
+            event[1] = "text";
+            break;
+        case "key":
+            // Key root number
+            event[2] = toRootNumber(event[2]);
+            break;
         case "note":
-            // Note name
-            //event[2] = toNoteNumber(event[2]);
+            // Note number
+            event[2] = toNoteNumber(event[2]);
             break;
     }
 
