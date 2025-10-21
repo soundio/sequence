21c3dcd35a1471e34ef769cdf78830e7e187174c
Stephen <stephband@cruncher.ch>
2025-10-21 12:07:18 +0200

    Parses dynamics

--- modules/parse/parse-gain.js
+++ modules/parse/parse-gain.js
@@ -1,8 +1,23 @@
 import toGain     from 'fn/to-gain.js';
 import parseValue from 'fn/parse-value.js';
 
+const dynamics = {
+    'fff': toGain(-5),
+    'ff':  toGain(-10),
+    'f':   toGain(-15),
+    'mf':  toGain(-20),
+    'mp':  toGain(-25),
+    'p':   toGain(-30),
+    'pp':  toGain(-35),
+    'ppp': toGain(-40)
+};
+
 export default parseValue({
-    '':   parseFloat,
-    'db': toGain,
-    'dB': toGain
+    '': parseFloat,
+    db: toGain,
+    dB: toGain,
+    catch: (string) => {
+        if (!dynamics[string]) throw new Error('Sequence: dynamic "' + string + '" not recognised');
+        return dynamics[string];
+    }
 });
